44a1bdcb7a6087dc0f01a9b5e989c8214fee6a9f
tungrg <18120258@student.hcmus.edu.vn>
2021-10-18 14:59:20 +0700

    2nd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,21 +3,46 @@ import com.company.math_expression;
 import java.util.Scanner;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
-	// write your code here
+        menu();
     }
     public static void menu(){
-        Scanner myObj = new Scanner(System.in);
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Calculator can only take number from -20 -> 20!!!");
         System.out.println("Enter the 1st number: ");
-        double _1st = myObj.nextDouble();
+        double first_num = myScanner.nextDouble();
+        if (first_num > - 20 && first_num <20) {
+            System.out.println("Ok!!");
+        }
+        else {
+            System.out.println("Error!!");
+            return;
+        }
         System.out.println("Enter the mathematical expression (+,-,*,/): ");
-        String exp = myObj.nextLine();
+        String mathExpress = myScanner.nextLine();
+        if (!mathExpress.equals("+") || !mathExpress.equals("-") || !mathExpress.equals("*")  || !mathExpress.equals("/"))
+        {
+            System.out.println("Ok!!");
+        }
+        else {
+            System.out.println("Error!!");
+            return;
+        }
         System.out.println("Enter the 2nd number: ");
-        double _2nd = myObj.nextDouble();
+        double second_num = myScanner.nextDouble();
+        if(second_num >-20 && second_num <20){
+            System.out.println("Ok!!");
+        }
+        else {
+        System.out.println("Error!!");
+        return;
+        }
         math_expression calculate = new math_expression();
-        calculate.set_number(_1st,_2nd, exp);
+        calculate.set_number(first_num,second_num, mathExpress);
+        System.out.println("Result: " + calculate.calculate());
 
     }
 }
--- src/com/company/math_expression.java
+++ src/com/company/math_expression.java
@@ -11,23 +11,32 @@ public class math_expression {
         expression = _exp;
     }
     double calculate(){
-        if (expression =="+")
+        if (expression.equals("+"))
         {
             return first_number + second_number;
         }
-        else if(expression == "-")
+        else if(expression.equals("-"))
         {
             return first_number + second_number;
         }
-        else if (expression == "*")
+        else if (expression.equals("*") )
         {
             return first_number * second_number;
         }
-        else if (expression == "/" && second_number !=0)
+        else if (expression.equals("/") && second_number !=0)
         {
             return first_number / second_number;
         }
-        else return 0; 
+        else if (expression.equals("/") && second_number ==0)
+        {
+            System.out.println("Can't not divide 0!!!");
+            return Double.POSITIVE_INFINITY;
+        }
+        else
+        {
+            System.out.println("Can't not implement this!!!");
+            return Double.POSITIVE_INFINITY;
+        }
     }
 }
 
